f308a13496fdedea345e89ebab7a88ce914ad9f5
LordMike <lordmike@iki.fi>
2014-07-29 09:40:57 +0300

    Added new PDF file links for publications.
    Signed-off-by: LordMike <lordmike@iki.fi>

--- publication.php
+++ publication.php
@@ -9,9 +9,11 @@
     <p>Publications of Lord Mike (a.k.a. Mikko Niemelä)</p>
     <p></p>
 	<p><a href="http://dx.doi.org/10.1364/OE.22.002324">Multiwavefront digital holographic television<br>
-	Optics Express, Vol. 22, Issue 3, pp. 2324-2336 (2014)</a></p>
+	Optics Express, Vol. 22, Issue 3, pp. 2324-2336 (2014)</a>
+	<a href="https://drive.google.com/file/d/0B3jJ9fTmWod3Z0lMb2pkX0IzdnM/edit?usp=sharing">unofficial PDF link</a></p>
 	<p><a href="http://dx.doi.org/10.1364/AO.53.003233">Partially coherent digital in-line holographic microscopy in characterization of a microscopic target<br>
 	Applied Optics, Vol. 53, Issue 15, pp. 3233-3240 (2014)</a>
-	<a href="https://drive.google.com/file/d/0B3jJ9fTmWod3RFZuRF9qYlB2MFU/edit?usp=sharing">PDF</a></p>
+	<a href="https://drive.google.com/file/d/0B3jJ9fTmWod3R2RucDY4N2FFZUk/edit?usp=sharing">unofficial PDF link</a>
+	<a href="https://drive.google.com/file/d/0B3jJ9fTmWod3RFZuRF9qYlB2MFU/edit?usp=sharing">old PDF link</a></p>
   </body>
 </html>
