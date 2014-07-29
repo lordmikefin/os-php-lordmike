a1485e62b26c642bda04045d383b4e21cfecfd73
LordMike <lordmike@iki.fi>
2014-07-29 09:54:55 +0300

    Fized publication links.
    Signed-off-by: LordMike <lordmike@iki.fi>

--- publication.php
+++ publication.php
@@ -10,9 +10,9 @@
     <p></p>
 	<p><a href="http://dx.doi.org/10.1364/OE.22.002324">Multiwavefront digital holographic television<br>
 	Optics Express, Vol. 22, Issue 3, pp. 2324-2336 (2014)</a>
-	<a href="https://drive.google.com/file/d/0B3jJ9fTmWod3Z0lMb2pkX0IzdnM/edit?usp=sharing">unofficial PDF link</a></p>
+	<a href="https://drive.google.com/file/d/0B3jJ9fTmWod3Z0lMb2pkX0IzdnM/edit?usp=sharing">unofficial PDF link</a> (© OSA)</p>
 	<p><a href="http://dx.doi.org/10.1364/AO.53.003233">Partially coherent digital in-line holographic microscopy in characterization of a microscopic target<br>
 	Applied Optics, Vol. 53, Issue 15, pp. 3233-3240 (2014)</a>
-	<a href="https://drive.google.com/file/d/0B3jJ9fTmWod3R2RucDY4N2FFZUk/edit?usp=sharing">unofficial PDF link</a>
+	<a href="https://drive.google.com/file/d/0B3jJ9fTmWod3UkVBY25GeldmNWs/edit?usp=sharing">unofficial PDF link</a> (© OSA)</p>
   </body>
 </html>
