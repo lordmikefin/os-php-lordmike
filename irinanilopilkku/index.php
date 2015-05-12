b02e36d8376b8ab46bc9aad8c0cae63083a876f7
LordMike <lordmike@iki.fi>
2015-05-12 14:56:25 +0300

    .

--- irinanilopilkku/index.php
+++ irinanilopilkku/index.php
@@ -7,11 +7,13 @@
 	<body>
 		<h1>UNDER CONSTRUCTION</h1>
 		<br>
-		## <p>Test site of Lord Mike (a.k.a. Mikko Niemelä)</p>
-		## <p>I'll try to up some content here :)</p>
-		## <br>
-		## <h1>UNDER CONSTRUCTION</h1>
-		## <p>test</p>
+		<!--
+		<p>Test site of Lord Mike (a.k.a. Mikko Niemelä)</p>
+		<p>I'll try to up some content here :)</p>
+		<br>
+		<h1>UNDER CONSTRUCTION</h1>
+		<p>test</p>
+		-->
 		<p>Created by Mikko Niemelä ( <img alt="" border="0" src="./lordmike.svg"> )</p>
 	</body>
 </html>
