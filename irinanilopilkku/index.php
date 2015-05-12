688831704a130cc65bf9fe5e8841d12a62fe1bac
LordMike <lordmike@iki.fi>
2015-05-12 14:50:06 +0300

    Added initial code for irinan example page.

---
+++ irinanilopilkku/index.php
@@ -0,0 +1,17 @@
+﻿<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Irinan ilopilkku</title>
+	</head>
+	<body>
+		<h1>UNDER CONSTRUCTION</h1>
+		<br>
+		## <p>Test site of Lord Mike (a.k.a. Mikko Niemelä)</p>
+		## <p>I'll try to up some content here :)</p>
+		## <br>
+		## <h1>UNDER CONSTRUCTION</h1>
+		## <p>test</p>
+		<p>Created by Mikko Niemelä ( <img alt="" border="0" src="./lordmike.svg"> )</p>
+	</body>
+</html>
